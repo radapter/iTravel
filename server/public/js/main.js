90cc35f14f821b9e7bde77ecacf7f64a33f30ced
Jennifer Wu <jenn.j.wu@gmail.com>
2015-04-12 10:44:52 -0700

    Add Contact Page, minor restyling on each page . adds contact page partial and to route in main.js . fix typos and minor rewording on home page

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -11,25 +11,14 @@ app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         // Home
         .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+
         // Pages
         .when("/login", {templateUrl: "partials/login.html", controller: "PageCtrl"})
         .when("/signup", {templateUrl: "partials/signup.html", controller: "PageCtrl"})
         .when("/travel", {templateUrl: "partials/travel.html", controller: "PageCtrl"})
         .when("/aboutus", {templateUrl: "partials/aboutus.html", controller: "PageCtrl"})
+		.when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+
         // else error
         .otherwise("/error", {templateUrl: "partials/error.html", controller: "PageCtrl"});
 }]);
-
-/*angular.module('iTravel', ['ui.router', 'ui.bootstrap'])
-	.config(function($stateProvider, $urlRouterProvider) {
-	    $urlRouterProvider.otherwise('/');
-	    
-	    // define states here...
-	    $stateProvider
-	        .state('xxx', {
-	            url: '/xxx',
-	            controller: 'xxx',
-	            templateUrl: 'xxx.html'
-	        });
-	});
-*/
