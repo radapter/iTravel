64bfeb738e276d0bf45cb41d228036e5421b1cbd
Yulin Ye <yulinye.yy@gmail.com>
2015-04-11 22:18:31 -0700

    create NavbarCtrl

--- server/public/js/controller/PageCtrl.js
+++ server/public/js/controller/PageCtrl.js
@@ -1,17 +1,5 @@
-app.controller('PageCtrl', function ($scope, $location, $http ) {
+angular.module('iTravelApp').controller('PageCtrl', function ($scope, $location, $http ) {
     console.log("Page Controller reporting for duty..");
     console.log($location.$$path);
-
-
-    //$scope.showStatus = false;
-
-    if($location.$$path === "/"){
-        console.log("in /");
-        $scope.showStatus = false;
-    } else {
-        console.log("in else");
-        $scope.showStatus = true;
-    }
-    console.log("status:"+$scope.showStatus);
 
 });
