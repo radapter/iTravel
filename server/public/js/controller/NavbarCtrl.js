d6c049839e89bf18b8af417385807d53ff856fb8
Yulin Ye <yulinye.yy@gmail.com>
2015-04-11 22:44:21 -0700

    fix typo

--- server/public/js/controller/NavbarCtrl.js
+++ server/public/js/controller/NavbarCtrl.js
@@ -2,23 +2,7 @@ angular.module('iTravelApp')
 .controller('NavbarCtrl', ['$scope', '$location', function ($scope, $location) {
     
     $scope.$on('$routeChangeSuccess', function(evt) {
-        console.log(evt);
         $scope.showStatus = ($location.$$path === "/") ? false : true;
-
-        console.log('$scope.showStatus', $scope.showStatus);
     });
-
-    //$scope.showStatus = false;
-
-    // if($location.$$path === "/"){
-    //     console.log("in /");
-    //     $scope.showStatus = false;
-    // } else {
-    //     console.log("in else");
-    //     $scope.showStatus = true;
-    // }
-    // console.log("status:"+$scope.showStatus);
-
-    
 
 }]);
--- server/public/js/controller/PageCtrl.js
+++ server/public/js/controller/PageCtrl.js
@@ -1,5 +1,4 @@
 angular.module('iTravelApp').controller('PageCtrl', function ($scope, $location, $http ) {
     console.log("Page Controller reporting for duty..");
     console.log($location.$$path);
-
 });
