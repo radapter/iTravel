cae086796208136ba203d8995c73bd38fbc8f323
Xiaoli Jiang <jiangxiaoli1104@gmail.com>
2015-04-11 23:13:02 -0700

    navbar styling fixed

--- server/public/js/controller/NavbarCtrl.js
+++ server/public/js/controller/NavbarCtrl.js
@@ -2,7 +2,7 @@ angular.module('iTravelApp')
 .controller('NavbarCtrl', ['$scope', '$location', function ($scope, $location) {
     
     $scope.$on('$routeChangeSuccess', function(evt) {
-        $scope.showStatus = ($location.$$path === "/") ? false : true;
+        $scope.showStatus = ($location.$$path === "/") ? true : false;
     });
 
 }]);
