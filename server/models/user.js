ec1a4861538636305b5a4b74e6529fccd2abd5d1
Xiaoli Jiang <jiangxiaoli1104@gmail.com>
2015-04-17 16:55:42 -0700

    todos for venue and user in each file

--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,11 @@
 'use strict';
 
-module.exports = 
+//module.exports =
+
+//TO BE DONE
+//add/modify/delete plans to user
+
+//add/modify/delete activities to plans
+
+//add/modofy/delete actitivity to activities
+
