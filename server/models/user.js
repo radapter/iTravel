e78377c7b10086d473f7e42191195a7d1c150264
Yulin Ye <yulinye.yy@gmail.com>
2015-04-17 23:46:46 -0700

    create authManageer and move MongoDB connection info to secrets.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,32 @@
 'use strict';
 
-//module.exports =
+var mongoose = require('../utils/mongoConnection');
+
+var userSchema = mongoose.Schema({
+    //user schema
+    //id: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    plans:[{
+        destAddr: String,
+        destLat: Number,
+        destLng: Number,
+        startDate: Date,
+        endDate: Date,
+        active: {type: Boolean, default: true },
+        activities:[{
+            venue: String,
+            startTime: String, //store time as string????
+            duration: Number,
+            category: String
+        }]
+    }]
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
 
 //TO BE DONE
 //add/modify/delete plans to user
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,38 +4,11 @@
 
 var express = require('express');
 var restify = require('express-restify-mongoose');
+var User = require('../models/user');
 var router = express.Router();
-//var user = require('../domain/user');
 
-var mongoose = require('../utils/mongoConnection');
 
-var Schema = mongoose.Schema;
-
-var User = new Schema({
-    //user schema
-    //id: { type: String, required: true },
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true },
-    plans:[{
-        destAddr: String,
-        destLat: Number,
-        destLng: Number,
-        startDate: Date,
-        endDate: Date,
-        active: {type: Boolean, default: true },
-        activities:[{
-            venue: String,
-            startTime: String, //store time as string????
-            duration: Number,
-            category: String
-        }]
-    }]
-});
-
-var UserModel = mongoose.model('User', User);
-
-restify.serve(router, UserModel);
+restify.serve(router, User);
 
 module.exports = router;
 
