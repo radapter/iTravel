ec91c16f778456db523b2b75bb32862928f8cc98
Jennifer Wu <jenn.j.wu@gmail.com>
2015-04-19 17:07:25 -0700

    Add Pages for Attraction, Restaurant, Hotel Selections, and Plan Drag/Drop Page . adds to ng-router . adds basic html pages (hardcoded content for now) . use "explore nearby" button to link to first page (attraction)

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -1,29 +1,36 @@
-'use strict';
+(function() {
+    'use strict';
 
-var app = angular.module('iTravelApp', [
-	'ngRoute'
-]);
+    var app = angular.module('iTravelApp', [
+        'ngRoute'
+    ]);
 
-/**
- * Configure the Routes
- */
-app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-        // Home
-        .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    /**
+     * Configure the Routes
+     */
+    app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            // Home
+            .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
 
-        // Pages
-        .when("/login", {templateUrl: "partials/login.html", controller: "PageCtrl"})
-        .when("/signup", {templateUrl: "partials/signup.html", controller: "PageCtrl"})
-        .when("/travel", {templateUrl: "partials/travel.html", controller: "PageCtrl"})
-        .when("/aboutus", {templateUrl: "partials/aboutus.html", controller: "PageCtrl"})
-		.when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
-        .when("/locations", {templateUrl: "partials/locations.html", controller: "PageCtrl"})
-        .when("/tech", {templateUrl: "partials/tech.html", controller: "PageCtrl"})
-        .when("/privacy", {templateUrl: "partials/privacy.html", controller: "PageCtrl"})
-        .when("/security", {templateUrl: "partials/security.html", controller: "PageCtrl"})
-        .when("/developer", {templateUrl: "partials/developer.html", controller: "PageCtrl"})
+            // Pages
+            .when("/login", {templateUrl: "partials/login.html", controller: "PageCtrl"})
+            .when("/signup", {templateUrl: "partials/signup.html", controller: "PageCtrl"})
+            .when("/attraction", {templateUrl: "partials/attraction.html", controller: "PageCtrl"})
+            .when("/restaurant", {templateUrl: "partials/restaurant.html", controller: "PageCtrl"})
+            .when("/hotel", {templateUrl: "partials/hotel.html", controller: "PageCtrl"})
+            .when("/plan", {templateUrl: "partials/plan.html", controller: "PageCtrl"})
+            .when("/travel", {templateUrl: "partials/travel.html", controller: "PageCtrl"})
+            .when("/aboutus", {templateUrl: "partials/aboutus.html", controller: "PageCtrl"})
+            .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+            .when("/locations", {templateUrl: "partials/locations.html", controller: "PageCtrl"})
+            .when("/tech", {templateUrl: "partials/tech.html", controller: "PageCtrl"})
+            .when("/privacy", {templateUrl: "partials/privacy.html", controller: "PageCtrl"})
+            .when("/security", {templateUrl: "partials/security.html", controller: "PageCtrl"})
+            .when("/developer", {templateUrl: "partials/developer.html", controller: "PageCtrl"})
 
-        // else error
-        .otherwise("/error", {templateUrl: "partials/error.html", controller: "PageCtrl"});
-}]);
+            // else error
+            .otherwise("/error", {templateUrl: "partials/error.html", controller: "PageCtrl"});
+    }]);
+
+})();
