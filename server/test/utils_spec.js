e0f54792d571a63e7911af4016d3af899e0800d8
Tuo Lei <leituo56@gmail.com>
2015-04-19 18:58:10 -0700

    resolve #8, 1. add search api for fourSquareProxy 2. add test case 3. give example to store foursquare key and secret in secrets.js(in test case)

--- server/test/utils_spec.js
+++ server/test/utils_spec.js
@@ -1,11 +1,15 @@
-var expect = require('chai').expect
+var expect = require('chai').expect;
+var secrets = require('../secrets');
 
-var fourSquare = (require('../utils/fourSquare'))("Z4K0IZ0P0UOLQ5DRTP4LLU32TJVTAP50MFKEKXOP5NAPFFEK", "JXZT5MFR54XBZFHLQ440UQGSRVXQNJ42C33QDH1VL2GA0YDD");
+var fourSquare = (require('../utils/fourSquareProxy'))(secrets.fourSquareId, secrets.fourSquareSecret);
+
 // utils/fourSquare.js, core API
 describe('utils/fourSquare.js, core API', function(){
+  // explore API
   describe('#explore', function(){
     it('should return result of ll:37.3,-121.88 ', function(done){
       fourSquare.explore({"ll":"37.3,-121.88"}, function(error, body){
+        expect(error).to.equal(null);
         expect(body).to.be.a('object');
         expect(body.meta.code).to.equal(200);
         expect(body.response.groups[0].items.length > 1).to.equal(true);
@@ -15,9 +19,25 @@ describe('utils/fourSquare.js, core API', function(){
 
     it('should return result of San Jose', function(done){
       fourSquare.explore({"near":"San Jose, CA"}, function(error, body){
+        expect(error).to.equal(null);
         expect(body).to.be.a('object');
         expect(body.meta.code).to.equal(200);
         expect(body.response.groups[0].items.length > 1).to.equal(true);
+        done();
+      });
+    });
+
+  });
+
+  // search API
+  describe('#search', function(){
+    it('should return result of KFC at Fremont', function(done){
+      fourSquare.search({"near":"Fremont, CA", "query":"KFC"}, function(error, body){
+        expect(error).to.equal(null);
+        expect(body).to.be.a('object');
+        expect(body.meta.code).to.equal(200);
+        expect(body.response.venues.length > 1);
+        expect(body.response.venues[0].name).to.equal("KFC");
         done();
       });
     });
