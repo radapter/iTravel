c11fa0cfc5c5cb71333482a028407d4b3c528ab5
Xiaoli Jiang <jiangxiaoli1104@gmail.com>
2015-04-21 15:58:20 -0700

    user profile page

--- server/public/js/controllers/UserShowCtrl.js
+++ server/public/js/controllers/UserShowCtrl.js
@@ -3,8 +3,50 @@
 
     angular.module('iTravelApp')
         .controller('UserShowCtrl', ['$scope', '$location', 'User', function($scope, $location, User) {
-            $scope.user = User.currentUser;
+
+            $scope.planCount = 0;
+            $scope.destinationCount = 0;
+            $scope.activityCount = 0;
+
+            //retrieve user
+            User.restore()
+                .then(function () {
+
+                    console.log($scope.user);
+
+                    $scope.user = User.currentUser;
+                    $scope.planCount = $scope.user.plans.length;
+                    $scope.activityCount = countActivities($scope.user);
+                    $scope.destinationCount = countDestinations($scope.user);
+
+
+                });
+
+
 
 
         }]);
+
+    function countActivities(user) {
+        if(!user) return 0;
+        var activityCount = 0;
+        for(var i=0; i< user.plans.length; i++){
+            activityCount += user.plans[i].activities.length;
+        }
+        return activityCount;
+    }
+
+
+    function countDestinations(user) {
+        if(!user) return 0;
+        var destinations = [];
+        for(var i=0; i< user.plans.length; i++){
+            if(!_.contains(destinations, user.plans[i].destName) ) {
+                destinations.push(user.plans[i].destName);
+            }
+        }
+        return destinations.length;
+    }
+
+
 })();
