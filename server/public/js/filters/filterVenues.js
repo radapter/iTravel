162a9c25a2a5be0c5d27330d86cff10c53afb35f
Jennifer Wu <jenn.j.wu@gmail.com>
2015-04-22 20:40:49 -0700

    Add venue.price.tier and venue.rating if they are empty for sorting . during venue filter in filterVenue.js, add values to these properties of Venue object so angular sort works as expected (no price assigned 0 and sorted to bottom, no rating assigned -1 and sorted to bottom and hidden from view) . remove attraction/restaurant/hotel links from header navbar

--- server/public/js/filters/filterVenues.js
+++ server/public/js/filters/filterVenues.js
@@ -7,13 +7,19 @@
                 if(!venues || !venues.length)
                     return;
 
-                //remove venues with no ratings - otherwise it messes up sorting
+                //add property values to venues to alleviate sorting problems
                 var firstPass = [];
                 for(var i = 0; i < venues.length; i++){
                     var venue = venues[i];
-                    if(venue.rating){
-                        firstPass.push(venue);
+                    //if venue does not have price, give it price tier of 0 to sort to bottom
+                    if(!venue.hasOwnProperty('price')){
+                        venue["price"] = {"tier": 0};
                     }
+                    //if venue does not have rating, give it -1 to sort to bottom
+                    if(!venue.rating){
+                        venue.rating = -1;
+                    }
+                    firstPass.push(venue);
                 }
 
                 //if there are no filterParams
