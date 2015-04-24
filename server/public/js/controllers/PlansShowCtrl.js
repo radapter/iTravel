79bd9ce7826cbd4bb2074154cfaa4377a21207fb
Xiaoli Jiang <jiangxiaoli1104@gmail.com>
2015-04-24 00:51:30 -0700

    ******WIP****** plan show page

---
+++ server/public/js/controllers/PlansShowCtrl.js
@@ -0,0 +1,35 @@
+(function() {
+    'use strict';
+
+    angular.module('iTravelApp')
+        .controller('PlansShowCtrl', ['$scope', '$routeParams', '$location', 'User', function($scope, $routeParams, $location, User) {
+
+            //retrieve user & plan
+            User.restore()
+                .then(function () {
+                    if(User.currentUser) {
+                        $scope.user = User.currentUser;
+                        console.log($scope.user);
+
+                        var planid = $routeParams.id;
+                        var plans = $scope.user.plans;
+                        for(var i = 0; i< plans.length; i++){
+                            console.log(plans[i]);
+                            if(plans[i]._id == planid) {
+                                $scope.plan = plans[i];
+                                console.log($scope.plan);
+                            }
+                        }
+
+                    }
+                });
+
+
+
+
+
+        }]);
+
+
+
+})();
