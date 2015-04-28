68ba1a438ba713b67e324b069ffb45c11fd71138
Xiaoli Jiang <jiangxiaoli1104@gmail.com>
2015-04-28 10:35:57 -0700

    change the hidewhenscroll to nontransparent when scroll

--- server/public/js/directives/hide-when-scroll.js
+++ server/public/js/directives/hide-when-scroll.js
@@ -14,16 +14,32 @@
 
 		angular.element(window).on('scroll', scrollHandler);
 
+		var isTrans = true;
 		function scrollHandler() {
 			var currScrollTop = $(this).scrollTop();
-			var allowHide = elem.hasClass('allow-hide');
-			//console.log('allowHide', allowHide);
-			if (allowHide && currScrollTop > lastScrollTop){
-			    elem.addClass('rd-top-n100');
-			} else {
-			    elem.removeClass('rd-top-n100');
+
+			//var allowHide = elem.hasClass('allow-hide');
+			////console.log('allowHide', allowHide);
+			//if (allowHide && currScrollTop > lastScrollTop){
+			//    elem.addClass('rd-top-n100');
+			//} else {
+			//    elem.removeClass('rd-top-n100');
+			//}
+			//lastScrollTop = currScrollTop;
+
+			//console.log(currScrollTop);
+			//when scroll over the landing image, change navbar from transparent to nontransparent
+			//fix navbar to top
+			if (currScrollTop > 580) {
+				elem.removeClass('top-nav-collapse');
+				isTrans = false;
 			}
-			lastScrollTop = currScrollTop;
+
+			//console.log(isTrans);
+			if (!isTrans && currScrollTop <= 580) {
+				elem.addClass('top-nav-collapse');
+			}
+
 		}
 	}
 })();
