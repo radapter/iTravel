eb567e6ae7aa031a654e56ca0112423aa5481a1b
Xiaoli Jiang <jiangxiaoli1104@gmail.com>
2015-04-27 23:24:11 -0700

    Add geolocation for explore nearby

--- server/public/js/controllers/HomeCtrl.js
+++ server/public/js/controllers/HomeCtrl.js
@@ -6,9 +6,11 @@
 
             $scope.isSubmitting = false;
 
+
             //select configs
             $scope.address = {};
             $scope.refreshAddresses = function(address) {
+                console.log(address);
                 var params = {address: address, sensor: false};
                 //console.log(params);
                 return $http.get(
@@ -53,5 +55,64 @@
                 }
 
             };
+
+
+            $scope.isSubmitting2 = false;
+            $scope.currentloc = function () {
+                $scope.isSubmitting2 = true;
+
+
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function(position){
+
+                        //console.log(position);
+                        //how to get the name
+
+                        var destlat = position.coords.latitude;
+                        var destLng = position.coords.longitude;
+
+                        var latlng = destlat+","+destLng;
+
+                        //use latlng to search in google geocode api
+                        var params = {latlng: latlng, sensor: false};
+
+                        $http.get(
+                            'https://maps.googleapis.com/maps/api/geocode/json',
+                            {params: params}
+                        ).then(function(response) {
+                                //console.log(response);
+
+                                //use the first result
+                                var destName = response.data.results[0].formatted_address;
+                                console.log(destName);
+
+                                var param = {
+                                    ll: latlng
+                                };
+                                console.log(param);
+                                Venue.explore(param)
+                                    .then(function success() {
+                                        $scope.isSubmitting2 = false;
+                                        //console.log(Venue.searchResults);
+                                        //console.log('get searchedResult successfully');
+
+                                        //create plan
+                                        //attrs: destName, destLat, destLng, startDate, endDate
+                                        Plan.create(destName, destlat, destLng);
+                                        console.log(Plan.tempPlan);
+
+                                        $location.url('/venueSelect');
+                                    }, function fail(err) {
+                                        console.log('get searchedResult failed. res:', err);
+                                    });
+
+                            });
+
+                    });
+                }
+
+            }
+
+
         }]);
 })();
