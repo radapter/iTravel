e90c65022bab19a69ddba5735e07ed192b2e7760
Tuo Lei <leituo56@gmail.com>
2015-08-27 22:08:01 -0700

    Add a basic feeling lucky feature. query by /foursquare/lucky?near=beijing&startDate=1438414209842&endDate=1438414209842 (these just random number, using timestamp) This is for API purpose, algorithm should be optimized basic ranking by rating basic ordering by longitude basic restaurant choosing by toping

--- server/domain/query.js
+++ server/domain/query.js
@@ -11,23 +11,93 @@
    */
   var Query = function(query){
     this.venues = [];
-    if(query.response.hasOwnProperty("groups")){
-      // Explore result
-      this.venues = query.response.groups[0].items.map(function(item){
-        return JSON.parse(JSON.stringify(item.venue));
-      });
-    }else if(query.response.hasOwnProperty("venues")){
-      // Search result
-      this.venues = JSON.parse(JSON.stringify(query.response.venues));
-    }else{
-      // Error
-      throw "Invalid query (from query.js)";
+
+    this.queryToVenues = function(query) {
+      var result;
+      if(query.response.hasOwnProperty("groups")){
+        // Explore result
+        result = query.response.groups[0].items.map(function(item){
+          return JSON.parse(JSON.stringify(item.venue));
+        });
+      }else if(query.response.hasOwnProperty("venues")){
+        // Search result
+        result = JSON.parse(JSON.stringify(query.response.venues));
+      }else{
+        // Error
+        throw "Invalid query (from query.js)";
+      }
+      return result;
+    }
+
+    this.appendVenues = function(venues) {
+      for (var newVenue in venues) {
+        var shared = false;
+        for (var currentVenue in this.venues) {
+          if(venues[newVenue].id == this.venues[currentVenue].id) {
+            shared = true;
+            break;
+          }
+        }
+        if(!shared) {
+          this.venues.push(venues[newVenue]);
+        }
+      }
+    }
+
+    this.appendData = function(query) {
+      var venues = this.queryToVenues(query);
+      this.appendVenues(venues);
+    }
+
+    // init
+    this.appendData(query);
+
+    /**
+     * Get a list of venue data
+     * @return {Array} list of venues of current query
+     */
+    this.getVenues = function(){
+      return this.venues;
     }
 
     /**
-     * return a list of venue data
+     * Get N places that most worth visiting.
+     * @param {Number}
+     * @return {Array} a list of venue data
      */
-    this.getVenues = function(){
+    this.selectTopVenues = function(num) {
+      // TODO this algorithm need optimization
+      // Currently its just a proof of concept sort by rating
+      var compareByRating = function (a,b) {
+        if (a.rating == undefined && b.rating == undefined)
+          return 0;
+        if (a.rating == undefined || a.rating < b.rating)
+          return -1;
+        if (b.rating == undefined || a.rating > b.rating)
+          return 1;
+        return 0;
+      }
+      this.venues = this.venues.sort(compareByRating).reverse().slice(0, num);
+      return this.venues;
+    }
+
+    /**
+     * Get the order of visiting clustered by day
+     * @param  {Number} days   num of days
+     * @return {Array}         venues reordered by visiting sequence
+     * Point(obj.location.lat,obj.location.lng)
+     */
+    this.sortVenuesByPath = function(days) {
+      // TODO this algorithm need optimization
+      // Currently its just a proof of concept sort by longitude
+      var compareByLng = function (a,b) {
+        if (a.location.lng < b.location.lng)
+          return -1;
+        if (a.location.lng > b.location.lng)
+          return 1;
+        return 0;
+      }
+      this.venues.sort(compareByLng);
       return this.venues;
     }
 
@@ -54,6 +124,18 @@
       });
       return result;
     }
+
+    this.getMetrics = function(){
+      var result = this.venues.map(function(venue){
+        return [
+          venue.rating,
+          venue.stats.checkinsCount,
+          venue.name
+        ];
+      });
+      return result;
+    }
+
     return this;
   };
 
--- server/routes/fourSquare.js
+++ server/routes/fourSquare.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var async = require('async');
 var express = require('express');
 var router = express.Router();
 var fourSquareProxy = (require('../utils/fourSquareProxy'))("Z4K0IZ0P0UOLQ5DRTP4LLU32TJVTAP50MFKEKXOP5NAPFFEK", "JXZT5MFR54XBZFHLQ440UQGSRVXQNJ42C33QDH1VL2GA0YDD");
@@ -23,6 +24,155 @@ router.get('/explore', function(req, res) {
     var venues = query.addCategoryHierarchy();
 
     res.json(venues);
+  });
+});
+
+/**
+ * Feeling lucky
+ */
+router.get('/lucky', function(req, res) {
+  var params = req.query;
+  params.venuePhotos = 1;
+  params.limit = 50;
+
+  var startDate;
+  if(params.startDate == undefined) {
+    startDate = new Date();
+  } else {
+    startDate = new Date(params.startDate);
+  }
+
+  var endDate;
+  if(params.endDate == undefined) {
+    endDate = new Date();
+    endDate.setDate(startDate.getDate() + 2);
+  } else {
+      endDate = new Date(params.endDate);
+  }
+
+  var plan = {};
+  plan.startDate = new Date(startDate.getTime());
+  plan.endDate = new Date(endDate.getTime());
+
+  var days = endDate.getDate() - startDate.getDate() + 1;
+  var schedule = [
+    {
+      activitiesType:'attractions',
+      start:new Date(1970, 0, 1, 8, 0, 0),
+      end:new Date(1970, 0, 1, 9, 30, 0)
+    },
+    {
+      activitiesType:'attractions',
+      start:new Date(1970, 0, 1, 10, 0, 0),
+      end:new Date(1970, 0, 1, 11, 30, 0)
+    },
+    {
+      activitiesType:'restaurants',
+      start:new Date(1970, 0, 1, 12, 0, 0),
+      end:new Date(1970, 0, 1, 13, 30, 0)
+    },
+    {
+      activitiesType:'attractions',
+      start:new Date(1970, 0, 1, 14, 0, 0),
+      end:new Date(1970, 0, 1, 15, 30, 0)
+    },
+    {
+      activitiesType:'attractions',
+      start:new Date(1970, 0, 1, 16, 0, 0),
+      end:new Date(1970, 0, 1, 17, 30, 0)
+    },
+    {
+      activitiesType:'restaurants',
+      start:new Date(1970, 0, 1, 18, 0, 0),
+      end:new Date(1970, 0, 1, 19, 30, 0)
+    }
+  ];
+
+  var visitPerDay = 4; // TODO calculate it;
+
+  async.parallel({
+    sights: function(done){
+      var sightParams = JSON.parse(JSON.stringify(params));
+      sightParams.section = 'sights';
+      fourSquareProxy.explore(sightParams, function(err, data) {
+        done(err, data);
+      });
+    },
+    arts: function(done){
+      var artParams = JSON.parse(JSON.stringify(params));
+      artParams.section = 'arts';
+      fourSquareProxy.explore(artParams, function(err, data) {
+        done(err, data);
+      });
+    },
+    outdoors: function(done){
+      var outdoorParam = JSON.parse(JSON.stringify(params));
+      outdoorParam.section = 'outdoors';
+      fourSquareProxy.explore(outdoorParam, function(err, data) {
+        done(err, data);
+      });
+    },
+    food: function(done){
+      var foodParam = JSON.parse(JSON.stringify(params));
+      foodParam.section = 'food';
+      fourSquareProxy.explore(foodParam, function(err, data) {
+        done(err, data);
+      });
+    }
+  },function(err, results) {
+      var attractions = new Query(results.sights);
+      attractions.appendData(results.arts);
+      attractions.appendData(results.outdoors);
+
+      attractions.selectTopVenues(days * visitPerDay);
+      attractions.sortVenuesByPath(days);
+      attractions = attractions.addCategoryHierarchy();
+      console.log("attractions:" + attractions.length);
+
+      var dinings = new Query(results.food);
+      dinings = dinings.addCategoryHierarchy();
+      console.log("restaurants:" + dinings.length);
+
+      plan.destName = results.sights.response.geocode.displayString;
+      plan.destLat = results.sights.response.geocode.center.lat;
+      plan.destLng = results.sights.response.geocode.center.lng;
+
+      var activities = [];
+      for(var day = 0; day < days; day++){
+        for(var idx in schedule) {
+          var item = schedule[idx];
+          item.start.setFullYear(startDate.getFullYear());
+          item.start.setMonth(startDate.getMonth());
+          item.start.setDate(startDate.getDate() + day);
+          item.end.setFullYear(startDate.getFullYear());
+          item.end.setMonth(startDate.getMonth());
+          item.end.setDate(startDate.getDate() + day);
+          console.log(item);
+          if(item.start < startDate || item.end > endDate)
+            continue;
+
+          var activity = {};
+          activity.start = new Date(item.start.getTime());
+          activity.end = new Date(item.end.getTime());
+
+          var typeName = item.activitiesType;
+          if (typeName == 'attractions' && attractions.length > 0) {
+            activity.activitiesType = 'attractions';
+            activity.venue = attractions.shift();
+            activity.title = activity.venue.name;
+            activities.push(activity);
+          } else if (typeName == 'restaurants' && dinings.length > 0) {
+            activity.activitiesType = 'restaurants';
+            // TODO find the nearest restaurant, now is just a random one
+            activity.venue = dinings.shift();
+            activity.title = activity.venue.name;
+            activities.push(activity);
+          }
+        }
+      }
+
+      plan.activities = activities;
+      res.json(plan);
   });
 });
 
